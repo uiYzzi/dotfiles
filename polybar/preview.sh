2c83a136cc155cd1a6ac5fe1e144f8d0b59dabb0
柚子 <40852301+uiYzzi@users.noreply.github.com>
2023-02-14 13:59:38 +0800

    update

--- polybar/preview.sh
+++ polybar/preview.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-# Add this script to your wm startup file.
+#!/usr/bin/env bash
 
 DIR="$HOME/.config/polybar"
 
@@ -10,5 +8,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+# Launch the preview bar
+polybar -q top -c "$DIR"/preview.ini &
+polybar -q mid -c "$DIR"/preview.ini &
+polybar -q bottom -c "$DIR"/preview.ini &
